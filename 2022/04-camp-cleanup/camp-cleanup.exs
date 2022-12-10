1f23b3ca35782d4631dde444afc538a6ba80edad
Steffen <mail@steffenhenschel.com>
2022-12-10 01:46:00 +0100

    day 5 & 6 in elixir

--- 2022/04-camp-cleanup/camp-cleanup.exs
+++ 2022/04-camp-cleanup/camp-cleanup.exs
@@ -1,38 +1,39 @@
 defmodule ElfCamp do
-  @inputFile "input.txt"
+  @input_file "input.txt"
 
   def cleanup() do
-    {:ok, input} = File.read(@inputFile)
+    {:ok, input} = File.read(@input_file)
 
-    parsed = input
-    |> String.split("\n")
-    |> Enum.filter(&(&1 != ""))
-    |> Enum.map(&(parseAssignmentPair(&1)))
+    parsed =
+      input
+      |> String.split("\n")
+      |> Enum.filter(&(&1 != ""))
+      |> Enum.map(&parse_assignment_pair(&1))
 
-    sumUp(parsed)
+    sum_up(parsed)
   end
 
-  defp parseAssignmentPair(line) do
+  defp parse_assignment_pair(line) do
     line
     |> String.split(["-", ","])
     |> Enum.map(&String.to_integer(&1))
   end
 
-  defp sumUp(list), do: sumUp(list, {0, 0})
+  defp sum_up(list), do: sum_up(list, {0, 0})
 
-  defp sumUp([[s1, e1, s2, e2] | tail], {overlapping, fullyOverlapping})
-  when (s1 <= s2 and e1 >= e2) or (s2 <= s1 and e2 >= e1) do
-    sumUp(tail, {overlapping + 1, fullyOverlapping + 1})
+  defp sum_up([[s1, e1, s2, e2] | tail], {overlapping, fully_overlapping})
+       when (s1 <= s2 and e1 >= e2) or (s2 <= s1 and e2 >= e1) do
+    sum_up(tail, {overlapping + 1, fully_overlapping + 1})
   end
 
-  defp sumUp([[s1, e1, s2, e2] | tail], {overlapping, fullyOverlapping})
-  when (e1 >= s2 and e1 <= e2) or (e2 >= s1 and e2 <= e1) do
-    sumUp(tail, {overlapping,  fullyOverlapping + 1})
+  defp sum_up([[s1, e1, s2, e2] | tail], {overlapping, fully_overlapping})
+       when (e1 >= s2 and e1 <= e2) or (e2 >= s1 and e2 <= e1) do
+    sum_up(tail, {overlapping, fully_overlapping + 1})
   end
 
-  defp sumUp([_head | tail], sum), do: sumUp(tail, sum)
+  defp sum_up([_head | tail], sum), do: sum_up(tail, sum)
 
-  defp sumUp([], r), do: r
+  defp sum_up([], r), do: r
 end
 
 ElfCamp.cleanup() |> IO.inspect()
