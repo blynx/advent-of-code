786e63a733ccd911d50a8b97d2ad547b49f7d7f3
Steffen Henschel <mail@steffenhenschel.com>
2025-12-09 15:22:53 +0100

    AOC 2025 Day 3: Lobby Part 2

--- 2025/Day03_Lobby/Sources/Day03_Lobby/Day03_Lobby.swift
+++ 2025/Day03_Lobby/Sources/Day03_Lobby/Day03_Lobby.swift
@@ -5,11 +5,12 @@ struct Lobby {
 		return rawInput.split(separator: "\n").map(String.init)
 	}
 	
-	func turnOnEscalator(banks: [String]) -> [Int] {
-		return banks.map(chooseBatteries(inBank:))
+	func turnOnEscalator(banks: [String], numOfBatteries: Int) -> [Int] {
+		return banks.map { bank in chooseBatteries(numOfBatteries, inBank: bank) }
 	}
 	
-	func chooseBatteries(inBank bank: String) -> Int {
+	@available(*, deprecated)
+	func chooseTwoBatteries(inBank bank: String) -> Int {
 		var tensBattery: Character = "0"
 		var onesBattery: Character = "0"
 		for index in bank.indices.dropLast(1) {
@@ -26,5 +27,28 @@ struct Lobby {
 			onesBattery = lastBattery
 		}
 		return tensBattery.wholeNumberValue! * 10 + onesBattery.wholeNumberValue!
+	}
+	
+	func chooseBatteries(_ numOfBatteries: Int, inBank bank: String) -> Int {
+		let chosenBatteryList = chooseBatteries(numOfBatteries, inBank: Array(bank)[...], into: [Character]())
+		return Int(String(chosenBatteryList))!
+	}
+	
+	func chooseBatteries(_ numOfBatteries: Int, inBank bank: ArraySlice<Character>, into chosenBatteries: [Character]) -> [Character] {
+		guard numOfBatteries > 0 else { return chosenBatteries }
+		let endIndex = bank.endIndex.advanced(by: -numOfBatteries)
+		let (maxBattery, maxBatteryIndex) = bank[...endIndex].maxWithIndex()
+		return chooseBatteries(numOfBatteries - 1,
+							   inBank: bank[maxBatteryIndex!.advanced(by: 1)...],
+							   into: chosenBatteries + [maxBattery!])
+	}
+}
+
+extension Collection where Element: Comparable {
+	func maxWithIndex() -> (item: Element?, index: Index?) {
+		if let index = self.indices.max(by: { self[$1] > self[$0] }) {
+			return (self[index], index)
+		}
+		return (nil, nil)
 	}
 }
--- 2025/Day03_Lobby/Tests/Day03_LobbyTests/Day03_LobbyTests.swift
+++ 2025/Day03_Lobby/Tests/Day03_LobbyTests/Day03_LobbyTests.swift
@@ -12,17 +12,37 @@ import Foundation
 
 	@Test func chooseBatteryTest() {
 		let lobby = Lobby()
-		#expect(lobby.chooseBatteries(inBank: "987654321111111") == 98)
-		#expect(lobby.chooseBatteries(inBank: "811111111111119") == 89)
-		#expect(lobby.chooseBatteries(inBank: "234234234234278") == 78)
-		#expect(lobby.chooseBatteries(inBank: "818181911112111") == 92)
+		#expect(lobby.chooseTwoBatteries(inBank: "987654321111111") == 98)
+		#expect(lobby.chooseTwoBatteries(inBank: "811111111111119") == 89)
+		#expect(lobby.chooseTwoBatteries(inBank: "234234234234278") == 78)
+		#expect(lobby.chooseTwoBatteries(inBank: "818181911112111") == 92)
 	}
 	
 	@Test("Part 1: Turn on escalator")
 	func turnOnEscalatorTest() {
 		let lobby = Lobby()
-		let banks = lobby.turnOnEscalator(banks: input)
+		let banks = lobby.turnOnEscalator(banks: input, numOfBatteries: 2)
 		#expect(banks.sum == 17445)
+	}
+	
+	@Test("Part 2: Turn on escalator with 12 batteries")
+	func turnOnEscalatorProperlyTest() {
+		let lobby = Lobby()
+		let banks = lobby.turnOnEscalator(banks: input, numOfBatteries: 12)
+		#expect(banks.sum == 173229689350551)
+	}
+	
+	@Test func chooseBatteriesTest() {
+		let lobby = Lobby()
+
+		#expect(lobby.chooseBatteries(1, inBank: "2413") == 4)
+		#expect(lobby.chooseBatteries(2, inBank: "818181911112111") == 92)
+		
+		#expect(lobby.chooseBatteries(12, inBank: "987654321111111") == 987654321111)
+		#expect(lobby.chooseBatteries(12, inBank: "811111111111119") == 811111111119)
+		#expect(lobby.chooseBatteries(12, inBank: "234234234234278") == 434234234278)
+		#expect(lobby.chooseBatteries(12, inBank: "818181911112111") == 888911112111)
+		
 	}
 }
 
